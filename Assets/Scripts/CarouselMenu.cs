52060f21ec2040faba8ac9c00d8c802c12e07d27
Xiaochen Zhang (Eric) <xiaochen.zhang@volvocars.com>
2020-09-21 12:58:36 +0200

    initial commit

---
+++ Assets/Scripts/CarouselMenu.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using TMPro;
+
+public class CarouselMenu : MonoBehaviour
+{
+    float selectionItemWidth;
+    float selectionItemGap;
+    int selectionItemCount;
+    float unitOffsetPixel;
+    float midIndex;
+    string selectedSceneTag;
+
+
+    ////////////////////////////////////////////////////////
+    /// I'm using a ScrollView to achieve a self looping Carousel Menu
+    ///
+    /// The menu items are currently manually arranged, planning to automate it later.
+    ///
+    /// For current example, there are actually 3 options.
+    ///
+    /// They are listed as below:
+    ///
+    /// 3 , 1 2 3 , 1 2 3 , 1 2 3 , 1
+    ///
+    /// We can find the items are repeated for 3 times. This is for giving selections while scrolling left and right.
+    ///
+    /// So I am reseting the index to the middel section after each animation. 
+    ////////////////////////////////////////////////////////
+
+    public void SelectItem()
+    {
+        GameObject selectedItem = EventSystem.current.currentSelectedGameObject;
+        selectedSceneTag = selectedItem.transform.GetChild(0).GetComponent<TMP_Text>().text;
+        int selectedItemIndex = selectedItem.transform.GetSiblingIndex();
+        StartCoroutine(SetSelectedItemWithAnim(selectedItemIndex));
+    }
+
+    public void LoadSelectedScene()
+    {
+        MainLoader.Instance.LoadScene(selectedSceneTag);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HorizontalLayoutGroup layoutGroup = GetComponent<HorizontalLayoutGroup>();
+        
+        selectionItemGap = layoutGroup.spacing;
+        selectionItemWidth = transform.GetChild(0).GetComponent<RectTransform>().sizeDelta.x;
+        Debug.Log("selectionItemWidth: " + selectionItemWidth);
+        selectionItemCount = transform.childCount;
+        unitOffsetPixel = selectionItemWidth + selectionItemGap;
+
+        midIndex = (selectionItemCount - 1) / 2f;
+        SetSelectedItem((int)midIndex);
+    }
+
+    void SetSelectedItem(int index)
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector2(unitOffsetPixel * (midIndex - index), 0f);
+        transform.GetChild(lastSelectionIndex).localScale = new Vector2(1f, 1f);
+        transform.GetChild(index).localScale = new Vector2(1.2f, 1.2f);
+        lastSelectionIndex = index;
+        selectedSceneTag = transform.GetChild(index).GetChild(0).GetComponent<TMP_Text>().text;
+    }
+
+    IEnumerator SetSelectedItemWithAnim(int index)
+    {
+        yield return StartCoroutine(AnimateSelection(index));
+        lastSelectionIndex = index;
+
+        // Reset the index to the middel section.
+        int actualOptionCount = (selectionItemCount - 2) / 3;
+        if ((index - 1) < actualOptionCount)
+        {
+            SetSelectedItem(index + actualOptionCount);
+        }
+        else if ((index - 1) > actualOptionCount * 2 - 1)
+        {
+            SetSelectedItem(index - actualOptionCount);
+        }
+    }
+
+    int lastSelectionIndex;
+    IEnumerator AnimateSelection(int index)
+    {
+        float journey = 0f;
+        float duration = 0.3f;
+
+        while (journey <= duration)
+        {
+            journey += Time.deltaTime;
+            float percent = Mathf.Clamp01(journey / duration);
+            gameObject.GetComponent<RectTransform>().anchoredPosition = Vector2.Lerp(gameObject.GetComponent<RectTransform>().anchoredPosition, new Vector2(unitOffsetPixel * (midIndex - index), 0f), percent);
+            transform.GetChild(lastSelectionIndex).localScale = Vector2.Lerp(transform.GetChild(lastSelectionIndex).localScale, new Vector2(1f, 1f), percent);
+            transform.GetChild(index).localScale = Vector2.Lerp(transform.GetChild(index).localScale, new Vector2(1.2f, 1.2f), percent);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/MainLoader.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainLoader : MonoBehaviour
+{
+    ////////////////////////////////////////////////////////
+    /// Singelton 
+    ////////////////////////////////////////////////////////
+    private static MainLoader _instance;
+
+    public static MainLoader Instance
+    {
+        get { return _instance; }
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        _instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    ////////////////////////////////////////////////////////
+    /// Addressables 
+    ////////////////////////////////////////////////////////
+    public Slider progressbar;
+    public Text size;
+    private AssetReference menuRef;
+    private AsyncOperationHandle opInProgress;
+
+    public void LoadScene(string tag)
+    {
+        Debug.Log("Addressables Procedure Started for Loading Scene: " + tag + "@" + Time.time);
+        Addressables.ClearDependencyCacheAsync(tag);
+        Addressables.InitializeAsync().Completed += delegate {
+            Debug.Log("Addressables.InitializeAsync.Completed:@" + Time.time);
+            Addressables.GetDownloadSizeAsync(tag).Completed += delegate (AsyncOperationHandle<long> obj)
+            {
+                Debug.Log("Addressables.GetDownloadSizeAsync.Completed:@" + Time.time);
+                float downloadSizeInMB = (float)obj.Result / 1024 / 1024;
+                Debug.Log("GetDownloadSizeAsync: " + obj.Result + " bytes, which is " + downloadSizeInMB + " MB");
+
+                if (size)
+                    size.text += System.Environment.NewLine + "Size of download: " + downloadSizeInMB.ToString();
+
+                Addressables.DownloadDependenciesAsync(tag).Completed += delegate (AsyncOperationHandle opDownloadDependencies)
+                {
+                    Debug.Log("Addressables.DownloadDependenciesAsync.Completed:" + opDownloadDependencies.Status + "@" + Time.time);
+                    Addressables.LoadSceneAsync(tag).Completed += delegate (AsyncOperationHandle<SceneInstance> opLoadScene) {
+                        Debug.Log("LoadSceneAsync.Completed: " + opLoadScene.Status + "@" + Time.time);
+                        if (opLoadScene.Status == AsyncOperationStatus.Succeeded)
+                        {
+                        }
+                    };
+                };
+            };
+        };
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadScene("default");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (opInProgress.IsValid())
+        {
+            if (progressbar)
+                progressbar.value = opInProgress.PercentComplete;
+        }
+    }
+}
